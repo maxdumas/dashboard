5caf499b55a3466fdc455b77806347f0be610ce8
Silage Pete <silagepete@protonmail.com>
2020-10-06 16:25:55 -0700

    main @ 7cd52f24b4755c1a4258a8605269a32bff70bcc8

---
+++ precache-manifest.a11ec5c0a95c45f5116704cec1425763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9da69b92bd288bc3bbfe4377b0d8cd8",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "564c83387cff58c545ba",
+    "url": "/dashboard/static/css/main.378358a7.chunk.css"
+  },
+  {
+    "revision": "7c1718e9de670c6f9833",
+    "url": "/dashboard/static/js/2.527de5c1.chunk.js"
+  },
+  {
+    "revision": "c0713bc4934e1b0aca8afcb2b1757c19",
+    "url": "/dashboard/static/js/2.527de5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "564c83387cff58c545ba",
+    "url": "/dashboard/static/js/main.17c3e7d7.chunk.js"
+  },
+  {
+    "revision": "6388236c331b6c879f55",
+    "url": "/dashboard/static/js/runtime-main.9f3afc0c.js"
+  }
+]);
